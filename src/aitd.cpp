f7cf21cc9264ee03f472e8ce8dda3274cc0d6db1
byfron <josecrubio82@gmail.com>
2018-01-05 10:23:19 +0100

    Adds build script of legacy code

---
+++ src/aitd.cpp
@@ -0,0 +1,18 @@
+#include "SDL.h"
+#include "common/textconsole.h"
+
+#include "fitd.h"
+#include "gfx.h"
+#include "resource.h"
+#include "osystem.h"
+#include "common.h"
+#include "version.h"
+#include "main_loop.h"
+#include "textes.h"
+#include "common/forbidden.h"
+
+using namespace Fitd;
+int main(int argc, char **argv) {
+	return runFitd();
+}
+
